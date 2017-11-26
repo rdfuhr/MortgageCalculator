3be0fdff7278fb33c806a1ee7c12e42e01778d62
Richard Fuhr <richard.fuhr@gmail.com>
2017-11-25 19:50:09 -0800

    Change "Unknown" to "SolveFor" as name for radio buttons.  Write so-far-do-nothing function UpdateGlobalSolveForBasedOnRadioButton();

--- MortgageCalculator.ts
+++ MortgageCalculator.ts
@@ -242,9 +242,17 @@ function initialLoanMinusLoanAsAFunctionOfMonthlyRate(i : number, M : Mortgage) 
     return L - P*a;
 }
 
+function UpdateGlobalSolveForBasedOnRadioButton()
+{
+
+}
+
 function HandleSolveForRadioButtonChange()
 {
+    UpdateGlobalSolveForBasedOnRadioButton();
 }
+
+//***************************** TESTS ******************************/
 
 // http://www.moneychimp.com/calculator/mortgage_calculator.htm
 // http://www.bankrate.com/calculators/mortgages/mortgage-calculator-b.aspx
